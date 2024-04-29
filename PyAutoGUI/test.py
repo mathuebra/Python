0ed38447ea692409775b75737aa2971ac74b47ca
mathuebra <mathuebra@gmail.com>
2024-04-29 09:06:01 -0300

    Atualizações de informação e teste

--- PyAutoGUI/test.py
+++ PyAutoGUI/test.py
@@ -1,11 +1,9 @@
 import pyautogui
 import time
+import sys
+import math
 
-time.sleep(3)
+num_points = sys.argv[1]
 
-youtube_button = pyautogui.center(pyautogui.locateOnScreen('image.png', grayscale=True, confidence= 0.8))
-
-pyautogui.moveTo(youtube_button, duration=pyautogui.MINIMUM_DURATION)
-pyautogui.mouseDown(button='left')
-time.sleep(0.05)
-pyautogui.mouseUp(button='left')
+for i in num_points:
+    
