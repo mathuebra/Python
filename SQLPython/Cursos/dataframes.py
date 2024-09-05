9b021d7ae005028eb60f52371bc28dbb40b28354
mathuebra <mathuebra@gmail.com>
2024-09-05 19:09:08 -0300

    Remoção de pasta

--- SQLPython/Cursos/dataframes.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-nome_cidade = pd.Series(['Maringá', 'Itabira', 'Uberlândia'])
-populacao = pd.Series([403.063, 120.904, 699.097])
-
-finaldf = pd.DataFrame({"Cidade": nome_cidade, "População": populacao})
-
-print(finaldf)
